3a59eb9e9a6ff5a92788049ea57f1ad2cb7c1925
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-01-15 21:19:57 +1100

    changed the function code

--- Develop/js/tests.js
+++ Develop/js/tests.js
@@ -1,12 +1,15 @@
 // Function to get the user cocktail choice
 function getCocktail() {
     // DOM variables
-    let cocktailInput = document.getElementById("");
-    let cocktailSubmit = document.getElementById("");
+    let cocktailInput = document.getElementById("cocktail-input");
+    let cocktailSubmit = document.getElementById("cocktail-submit-btn");
 
     // main logic
     cocktailSubmit.addEventListener("click", function() {
-        cocktail = cocktailInput.ariaValueMax;
+        cocktail = cocktailInput.value;
+        console.log(cocktailInput);
     });
     return cocktail;
 }
+
+getCocktail();
