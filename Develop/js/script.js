8758906873e1b08e78a9261a234e4ce8ff5d7ca1
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-01-16 21:43:31 +1100

    fixed error in code

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,5 +1,6 @@
 // Global variables for the api data
 let API_KEY = "1";
+let cocktail = "";
 // let apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`;
 
 // global variables for DOM elements
@@ -7,17 +8,16 @@ let cocktailSubmitButton = document.getElementById("cocktail-submit-btn");
 
 // Function to get the user input and handle button click
 cocktailSubmitButton.addEventListener("click", function() {
-    let cocktail = "";
     let cocktailInput = document.getElementById("cocktail-input");
     let cocktailValue = cocktailInput.value;
     cocktail = cocktailValue.toLowerCase();
     console.log(cocktail);
-    getCocktailApi(cocktail);
-})
+    getCocktailApi();
+});
 
 
 // Function to fetch the cocktail that was searched
-function getCocktailApi(cocktail) {
+function getCocktailApi() {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
     .then(function(repsonse) {
         if (repsonse.ok) {
