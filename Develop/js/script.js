0258af3cb2383f8eb18e6efd39373df24721fc0e
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-01-17 19:17:31 +1100

    added code to filtrer returned data to only what the search word was and added the code to a larger function called cocktail()

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,30 +1,42 @@
-// Global variables for the api data
-let API_KEY = "1";
-let cocktail = "";
-// let apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`;
 
-// global variables for DOM elements
-let cocktailSubmitButton = document.getElementById("cocktail-submit-btn");
+// Function to handle all code relating to getting cocktails
+function cocktail() {
+  // Variables for the api data
+  let cocktail = "";
 
-// Function to get the user input and handle button click
-cocktailSubmitButton.addEventListener("click", function() {
+  // Variables for DOM elements
+  let cocktailSubmitButton = document.getElementById("cocktail-submit-btn");
+
+  // Function to get the user input and handle button click
+  cocktailSubmitButton.addEventListener("click", function () {
     let cocktailInput = document.getElementById("cocktail-input");
     let cocktailValue = cocktailInput.value;
-    cocktail = cocktailValue.toLowerCase();
+    cocktailValue = cocktailValue.toLowerCase();
+    cocktail = cocktailValue.charAt(0).toUpperCase() + cocktailValue.slice(1)
     console.log(cocktail);
     getCocktailApi();
-});
+  });
 
-
-// Function to fetch the cocktail that was searched
-function getCocktailApi() {
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
-    .then(function(repsonse) {
-        if (repsonse.ok) {
-            repsonse.json()
-            .then(function(data) {
-                console.log(data);
-            });
-        }
+  // Function to fetch the cocktail that was searched
+  function getCocktailApi() {
+    fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
+    ).then(function (repsonse) {
+      if (repsonse.ok) {
+        repsonse.json().then(function (data) {
+            for (let i=0; i<data.drinks.length; i++) {
+                if (data.drinks[i].strDrink == cocktail) {
+                    sendCocktail(data.drinks[i]);
+                }
+            }
+        });
+      }
     });
+  }
 }
+
+function sendCocktail(attr) {
+    console.log(attr);
+}
+
+cocktail();
