be8367d29236c05aee5d94c785d2a96b6810a4a0
Peter Gryllis <petergryllis@riacci.com>
2022-01-17 21:33:10 +1100

    updates to js files

--- Develop/js/tests.js
+++ Develop/js/tests.js
@@ -1,6 +1,6 @@
 
 let language = "";
-let cocktail = "";
+// let cocktail = "";
 
 // global variables for DOM elements
 let languageSubmitButton = document.getElementById("language-submit-btn");
@@ -11,33 +11,33 @@ languageSubmitButton.addEventListener("click", function() {
     let languageValue = languageInput.value;
     language = languageValue.toLowerCase();
     console.log(language);
-    getLanguageApi();
+    getTranslationApi("hello world");
 });
 
-function getLanguageApi(lan) {
-fetch("https://microsoft-translator-text.p.rapidapi.com/languages?api-version=3.0", {
-	"method": "GET",
-	"headers": {
-        "accept-language": lan,
-		"x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
-		"x-rapidapi-key": "f0eba571b5mshc9cca1ba3878dc8p1184e2jsn62f675d478a7"
-	}
-})
-.then(response => {
-	response.json()
-.then(data => {
-        console.log(data.url);
-})
-.catch(err => {
-	console.error(err);
-})});
+// function getLanguageApi(lan) {
+// fetch("https://microsoft-translator-text.p.rapidapi.com/languages?api-version=3.0", {
+// 	"method": "GET",
+// 	"headers": {
+//         "accept-language": lan,
+// 		"x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
+// 		"x-rapidapi-key": "f0eba571b5mshc9cca1ba3878dc8p1184e2jsn62f675d478a7"
+// 	}
+// })
+// .then(response => 
+// 	response.json())
+// .then(data => {
+//         console.log(data);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
+// }
 
-
-getLanguageApi(language);
+// getLanguageApi(language);
 
 
 function getTranslationApi(text) {
-fetch(`https://microsoft-translator-text.p.rapidapi.com/translate?to=%3CREQUIRED%3E&api-version=3.0&profanityAction=NoAction&textType=plain&to=de&to=it&to=fr`, {
+fetch(`https://microsoft-translator-text.p.rapidapi.com/translate?api-version=3.0&profanityAction=NoAction&textType=plain&to=de&to=it&to=fr`, {
 	"method": "POST",
 	"headers": {
 		"content-type": "application/json",
@@ -52,12 +52,37 @@ fetch(`https://microsoft-translator-text.p.rapidapi.com/translate?to=%3CREQUIRED
 })
 .then(response => {
 	console.log(response);
+    return response.json()
+})
+.then(data => {
+    console.log(data);
 })
 .catch(err => {
 	console.error(err);
-})};
+});
+}
 
-getTranslationApi(language);
+//
+fetch("https://microsoft-translator-text.p.rapidapi.com/translate?to=fr&api-version=3.0&profanityAction=NoAction&textType=plain", {
+	"method": "POST",
+	"headers": {
+		"content-type": "application/json",
+		"x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
+		"x-rapidapi-key": "f0eba571b5mshc9cca1ba3878dc8p1184e2jsn62f675d478a7"
+	},
+	"body": [
+		{
+			"Text": "I would really like to drive your car around the block a few times."
+		}
+	]
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+// getTranslationApi(language);
 
 //https://microsoft-translator-text.p.rapidapi.com/languages?api-version=3.0//
-//${text}//
