3b284a23aaf03de1784a8d0f4b98f605fa3d2c35
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-01-18 18:55:55 +1100

    added comments as instructions for work

--- Develop/js/tests.js
+++ Develop/js/tests.js
@@ -1,20 +1,34 @@
-let text = "Hello World";
-let toLanguage = "es";
-let fromLanguage = "en";
+function language() {
+    // make a list of acceptable languages (array)
+    // filter languages to code (eg spanish = es)
+    // get the cocktail data (cocktail variable)
+    // translate the cocktail data
+    // add the cocktail data to the output box
+  let cocktail = {
+      name: "margarita",
+      ingredient1: "vodka",
+      ingredient2: "salt",
+      instructions: "Stir the ingredients together"
+  };
+  let toLanguage = "es";
+  let fromLanguage = "en";
 
-fetch(`https://fast-translate.p.rapidapi.com/fastTranslate/translate?text=${text}&langDest=${toLanguage}&langOrigin=${fromLanguage}`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "fast-translate.p.rapidapi.com",
-		"x-rapidapi-key": "e796e5b386msh092aba973e5001ap1f4c3djsn150ad429e4a2"
-	}
-})
-.then(function(res) {
-    res.json()
-    .then(function(data) {
+  fetch(
+    `https://fast-translate.p.rapidapi.com/fastTranslate/translate?text=${text}&langDest=${toLanguage}&langOrigin=${fromLanguage}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "fast-translate.p.rapidapi.com",
+        "x-rapidapi-key": "e796e5b386msh092aba973e5001ap1f4c3djsn150ad429e4a2",
+      },
+    }
+  )
+    .then(function (response) {
+      response.json().then(function (data) {
         console.log(data);
+      });
     })
-})
-.catch(err => {
-	console.error(err);
-});
+    .catch((err) => {
+      console.error(err);
+    });
+}
