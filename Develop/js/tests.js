7a384c151386c844cbbf1e2996fcbb90999c6f3b
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-01-18 20:21:56 +1100

    get data from storage to be sent to api

--- Develop/js/tests.js
+++ Develop/js/tests.js
@@ -1,40 +1,42 @@
-function language() {
+function language() {  
+
+    let data = ""
+    // Get data from local storage
+    function getLocalStorage() {
+        let storedValue = JSON.parse(localStorage.getItem("cocktail"));
+        data = storedValue;
+        console.log(data);
+      }
+      getLocalStorage();
+
     // make a list of acceptable languages (array)
-    // filter languages to code (eg spanish = es)
-    // get the cocktail data (cocktail variable)
-    // translate the cocktail data
-    // add the cocktail data to the output box
+  let language = "";
 
+  // Variables for DOM elements
+  let languageSubmitButton = document.getElementById("language-submit-btn");
 
-  let listOfLanguages= ["spanish", "german"]
-  
-  let languageButton= document.getElementById("language-submit-btn")
-
-  languageButton.addEventListener("click", function () {
-    let userChoice= document.getElementById ("language-input")
-    console.log(userChoice.value)
-    console.log ("working")
-  })
-
-  
-
-
-
-
-
-  
-
-  let cocktail = {
-      name: "margarita",
-      ingredient1: "vodka",
-      ingredient2: "salt",
-      instructions: "Stir the ingredients together"
-  };
+  // Function to get the user input and handle button click
+  languageSubmitButton.addEventListener("click", function () {
+    let languageInput = document.getElementById("language-input");
+    let languageValue = languageInput.value;
+    languageValue = languageValue.toLowerCase();
+    language = languageValue.charAt(0).toUpperCase() + languageValue.slice(1);
+    // console.log(language);
+    languageInput.value = "";
+  });
+  // filter languages to code (eg spanish = es)
+  if (language == "spanish") {
+      toLanguage = "es"
+  }
+  // get the cocktail data (cocktail variable)
+  // translate the cocktail data
+  // add the cocktail data to the output box
+  let cocktail = data;
   let toLanguage = "es";
   let fromLanguage = "en";
 
   fetch(
-    `https://fast-translate.p.rapidapi.com/fastTranslate/translate?text=${text}&langDest=${toLanguage}&langOrigin=${fromLanguage}`,
+    `https://fast-translate.p.rapidapi.com/fastTranslate/translate?text=${cocktail}&langDest=${toLanguage}&langOrigin=${fromLanguage}`,
     {
       method: "GET",
       headers: {
@@ -45,10 +47,13 @@ function language() {
   )
     .then(function (response) {
       response.json().then(function (data) {
-        console.log(data);
+        console.log("Translated: \n" + data);
       });
     })
     .catch((err) => {
       console.error(err);
     });
+    
 }
+
+language();
