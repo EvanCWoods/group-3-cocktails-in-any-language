17b41f97617d6c66e85bd3100a6122d119cba3c0
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-01-19 12:43:49 +1100

    added logic to the language.js file to handle the data and translations, displaying data. Need to finish accounting for undefined or null measurement values associated with ingredients.

--- Develop/js/language.js
+++ Develop/js/language.js
@@ -1,8 +1,32 @@
 function language() {
 
-    function getTranslation() {
+    // Function to get the data object from local storage
+    function getLocalStorage() {
+        let storedValue = JSON.parse(localStorage.getItem("cocktail"));
+        let cocktailName = storedValue.strDrink;
+        let cocktailInstructions = storedValue.strInstructions;
+        let cocktailIngredients = [];
+        let cocktailMeasurements = [];
 
-        let text = "Hello World";
+        for (let i=0; i<15; i++) {
+            if (storedValue["strIngredient" + i] != null) {
+                cocktailIngredients.push(storedValue["strIngredient" + i]);
+            }
+            if (storedValue["strMeasure" + i] != null) {
+                cocktailMeasurements.push(storedValue["strMeasure" + i]);
+            }
+        }
+
+        let cocktail = cocktailName + "~" + cocktailInstructions + "~" + cocktailMeasurements.join(",") + "~" + cocktailIngredients.join(",");
+        getTranslation(cocktail);
+      }
+      getLocalStorage();
+
+
+    // Function to get the translation from the api
+    function getTranslation(value) {
+
+        let text = value;
         let fromLanguage = "";
         let toLanguage = "es";
 
@@ -19,9 +43,38 @@ function language() {
               return response.json();
             })
             .then(function (data) {
-              console.log(data);
+              splitTranslation = data.translated_text.split("~");
+              console.log(splitTranslation);
+              displayData(splitTranslation);
             })
     }
-    getTranslation();
+
+    function displayData(value) {
+
+        // Get the DOM elements
+        let nameEl = document.getElementById("translated-cocktail-name");
+        let instructionsEl = document.getElementById("translated-instructions");
+        let ingredientsContainer = document.getElementById("translated-ingredients-list");
+
+        // Clear the placeholder value
+        ingredientsContainer.innerHTML = "";
+
+        // Destructure the data into individual variables
+        let cocktailName = value[0];
+        let cocktailInstructions = value[1];
+        let cocktailMeasurements = value[2].split(",");
+        let cocktailIngredients = value[3].split(",");
+
+        // Assign the data values to the DOM elements
+        nameEl.textContent = cocktailName;
+        instructionsEl.textContent = cocktailInstructions;
+
+        for (let i=0; i<cocktailIngredients.length; i++) {
+            let listItem = document.createElement("li");
+            listItem.classList.add("cocktail-ingredient");
+            listItem.textContent = cocktailMeasurements[i] + " " + cocktailIngredients[i];
+            ingredientsContainer.appendChild(listItem);
+        }
+    }
 }
 language();
